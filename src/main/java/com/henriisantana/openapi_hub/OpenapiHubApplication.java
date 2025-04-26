066ce78381957ac2d27dc68db713c0966696cb09
Henrique Santana <idd_hsantos@uolinc.com>
2025-04-25 22:25:19 -0300

    first commit

---
+++ src/main/java/com/henriisantana/openapi_hub/OpenapiHubApplication.java
@@ -0,0 +1,13 @@
+package com.henriisantana.openapi_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henriisantana/openapi_hub/OpenapiHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriisantana.openapi_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
